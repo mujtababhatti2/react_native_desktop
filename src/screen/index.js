82b87fd7e5abe1151b305337927b8fad0701d3a1
Mujtaba Bhatti <ghulammujtaba908@gmail.com>
2025-03-24 13:55:36 +0500

    new project

---
+++ src/screen/index.js
@@ -0,0 +1,4 @@
+import Login from "./auth/login";
+export {
+    Login
+}
---
+++ windows/TestDesktop/ReactPackageProvider.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "winrt/Microsoft.ReactNative.h"
+
+namespace winrt::TestDesktop::implementation
+{
+    struct ReactPackageProvider : winrt::implements<ReactPackageProvider, winrt::Microsoft::ReactNative::IReactPackageProvider>
+    {
+    public: // IReactPackageProvider
+        void CreatePackage(winrt::Microsoft::ReactNative::IReactPackageBuilder const &packageBuilder) noexcept;
+    };
+} // namespace winrt::TestDesktop::implementation
+
